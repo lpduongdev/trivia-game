e30088dacc7cd1062e5720cdb4eb769e0039d1a9
niarvn <rainvn.cb@gmail.com>
2023-01-05 14:06:55 +0700

    Initial Commit

---
+++ Trivia Game/Trivia_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Trivia_GameApp.swift
+//  Trivia Game
+//
+//  Created by Rain on 05/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct Trivia_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
